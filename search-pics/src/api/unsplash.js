ca04cfe14060b600b2b4bd291bb436c092f8b2a2
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-24 21:22:27 +0530

    alter in api

--- search-pics/src/api/unsplash.js
+++ search-pics/src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-axios.create({
+export default axios.create({
     baseURL:'https://api.unsplash.com/',
     headers:{
         Authorization:'Client-ID X4aVEEcQOdQFc-tuhPQufHJ8m43k7IX9XwzdFymUnco'
