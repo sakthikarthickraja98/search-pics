1a40aab315ff5da063f24ca3431f234eb854b4de
sakthikarthickraja98 <60427728+sakthikarthickraja98@users.noreply.github.com>
2021-06-25 11:17:05 +0530

    alternation for image list

--- search-pics/src/components/imagelist.js
+++ search-pics/src/components/imagelist.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Imagelist = (props) => {
-    const images = props.Image.map((image) =>{
-       return <img key={image.id} src={image.urls.regular} alt={image.description} />
+    const images = props.Image.map(({id,urls,description}) =>{
+       return <img key={id} src={urls.regular} alt={description} />
     });
 
     return(
